d7b7108049d25d77d9097fd9708857ed4e18f673
Imran <imran@sawitpro.com>
2024-11-28 10:41:59 +0700

    Committed project creation
    Committed project creation

---
+++ Ebuddy/Ebuddy/EbuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EbuddyApp.swift
+//  Ebuddy
+//
+//  Created by Imran Ali Mohammed on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EbuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
